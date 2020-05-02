edbb940bdceabbe638de01015c479214db0fa2c1
muratkilinc <tarum44@gmail.com>
2020-05-02 15:33:37 -0500

    RunnerClasses

--- src/test/java/com/briteERP/runners/CucumberRunner.java
+++ src/test/java/com/briteERP/runners/CucumberRunner.java
@@ -1,4 +1,23 @@
 package com.briteERP.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/briteERP/step_definitions",
+        features = "src/test/resources/features",
+        dryRun =false,
+        strict = false,
+        tags="",
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber1.json",
+                "rerun:target/rerun.txt"
+        }
+
+)
+
 public class CucumberRunner {
 }
