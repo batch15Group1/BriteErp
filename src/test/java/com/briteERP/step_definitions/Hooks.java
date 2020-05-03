3d4dbc5ee2c1f752f0fd7ad03da9dedfdbb6514f
nurandemir <nurum.demir@gmail.com>
2020-05-03 00:44:17 -0500

    add hook and config.properties

--- src/test/java/com/briteERP/step_definitions/Hooks.java
+++ src/test/java/com/briteERP/step_definitions/Hooks.java
@@ -1,4 +1,33 @@
 package com.briteERP.step_definitions;
 
+
+import com.briteERP.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 public class Hooks {
+
+    @Before
+    public void setup(){
+        System.out.println("Test setup");
+        Driver.getDriver().manage().window().maximize();
+    }
+
+
+    @After
+    public void tearDown(Scenario scenario){
+        //how to check if scenario failed
+        if(scenario.isFailed()){
+            TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+            //get scenarios output as array byte
+            byte[] image = takesScreenshot.getScreenshotAs(OutputType.BYTES);
+            //attach screenshot to the report
+            scenario.embed(image,"image/png", scenario.getName());
+        }
+        System.out.println("Test clean up");
+        Driver.closeDriver();
+    }
 }
