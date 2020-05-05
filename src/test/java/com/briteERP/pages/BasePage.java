d1a36d577721d0044cda33732747020b6de8063a
onuralpd <58891911+onuralpd@users.noreply.github.com>
2020-05-04 23:20:11 -0500

    first

--- src/test/java/com/briteERP/pages/BasePage.java
+++ src/test/java/com/briteERP/pages/BasePage.java
@@ -1,5 +1,13 @@
 package com.briteERP.pages;
 
+import com.briteERP.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public abstract class BasePage {
+
+    protected WebDriver driver= Driver.getDriver();
+    protected WebDriverWait wait=new WebDriverWait(driver,25);
+
 
 }
